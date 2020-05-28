e884db33bcb7ba9e94e8dcd675246dd227237ed3
richasrianti <64759854+richasrianti@users.noreply.github.com>
2020-05-28 21:40:09 +0700

    project 3
    Signed-off-by: richasrianti <64759854+richasrianti@users.noreply.github.com>

---
+++ app/src/main/java/com/example/project3/About.java
@@ -0,0 +1,17 @@
+package com.example.project3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("About");
+    }
+}
+
